6e258a36b2faff49737c5031889cdb03402dff47
wwagner4 <wwagner4@gmail.com>
2020-02-01 19:24:18 +0100

    added string formatter

--- src/main/kotlin/at/itsv/ke/FormatterServiceImpl.kt
+++ src/main/kotlin/at/itsv/ke/FormatterServiceImpl.kt
@@ -5,15 +5,20 @@ import java.util.*
 
 class FormatterServiceImpl {
 
-    fun f(value: Double?, locale: Locale = Locale.GERMAN): String {
+    fun fmtDouble(value: Double?, locale: Locale = Locale.GERMAN): String {
         if (value == null) return ""
         return "%.2f".format(locale, value)
     }
 
-    fun f(value: Date?): String {
+    fun fmtDate(value: Date?): String {
         if (value == null) return ""
         val sdf = SimpleDateFormat("dd.MM.yyyy")
         return sdf.format(value)
+    }
+
+    fun fmtString(value: String?): String {
+        if (value == null) return ""
+        return value.trim()
     }
 
 }
--- src/test/kotlin/at/itsv/ke/TestFormatterServiceImpl.kt
+++ src/test/kotlin/at/itsv/ke/TestFormatterServiceImpl.kt
@@ -25,7 +25,7 @@ class TestFormatterServiceImpl {
     )
     @ParameterizedTest(name = "{0} is formatted to {1}")
     internal fun formatDoubelGerman(input: Double, expected: String) {
-        Assertions.assertEquals(expected, formatter.f(input, Locale.GERMAN)) {
+        Assertions.assertEquals(expected, formatter.fmtDouble(input, Locale.GERMAN)) {
             "$input formatted GERMAN should equal $expected"
         }
     }
@@ -43,27 +43,49 @@ class TestFormatterServiceImpl {
     )
     @ParameterizedTest(name = "{0} is formatted to {1}")
     internal fun formatDoubelEn(input: Double, expected: String) {
-        Assertions.assertEquals(expected, formatter.f(input, Locale.ENGLISH)) {
+        Assertions.assertEquals(expected, formatter.fmtDouble(input, Locale.ENGLISH)) {
             "$input formatted EN should equal $expected"
         }
+    }
+
+    @CsvSource(
+        value = [
+            "hallo|hallo",
+            "karl |karl",
+            " eva|eva",
+            "jack in the bottle|jack in the bottle",
+            "christmas tree |christmas tree"],
+        delimiter = '|'
+    )
+    @ParameterizedTest(name = "{0} is formatted to {1}")
+    internal fun formatString(input: String, expected: String) {
+        Assertions.assertEquals(expected, formatter.fmtString(input)) {
+            "$input formatted should equal $expected"
+        }
+    }
+
+    @Test
+    fun testStringNull() {
+        val v: String? = null
+        Assertions.assertEquals("", formatter.fmtString(v))
     }
 
     @Test
     fun testDateNullEn() {
         val v: Double? = null
-        Assertions.assertEquals("", formatter.f(v, Locale.ENGLISH))
+        Assertions.assertEquals("", formatter.fmtDouble(v, Locale.ENGLISH))
     }
 
     @Test
     fun testDateNullGerman() {
         val v: Double? = null
-        Assertions.assertEquals("", formatter.f(v, Locale.GERMAN))
+        Assertions.assertEquals("", formatter.fmtDouble(v, Locale.GERMAN))
     }
 
     @Test
     fun testDateNullDefault() {
         val v: Double? = null
-        Assertions.assertEquals("", formatter.f(v))
+        Assertions.assertEquals("", formatter.fmtDouble(v))
     }
 
     @CsvSource(
@@ -81,8 +103,9 @@ class TestFormatterServiceImpl {
             val ld = LocalDate.of(year, month, day)
             return Date.from(ld.atStartOfDay(ZoneId.systemDefault()).toInstant())
         }
+
         val date = dat(year, month, day)
-        Assertions.assertEquals(expected, formatter.f(date)) {
+        Assertions.assertEquals(expected, formatter.fmtDate(date)) {
             "$year $month $day formatted should equal $expected"
         }
     }
