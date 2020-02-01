20af100b8b1587e25373ae003b12ae694da96356
wwagner4 <wwagner4@gmail.com>
2020-02-01 14:21:07 +0100

    added parameterized test

--- src/test/kotlin/at/itsv/ke/TestDummy.kt
+++ src/test/kotlin/at/itsv/ke/TestDummy.kt
@@ -1,12 +1,38 @@
 package at.itsv.ke
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class TestDummy {
-    
-    @Test
-    fun testNotOk() {
-        throw IllegalStateException("OK")
+
+    fun squared(value: Double): Double {
+        return value * value
     }
-    
+
+    @Test
+    fun testSquared() {
+        assertEquals(4.0, squared(2.0))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "0|0",
+            "1|1",
+            "2|4",
+            "3.0|9.0",
+            "2.222|4.937284"],
+        delimiter = '|'
+    )
+    @DisplayName("value {0} squared = {1}")
+    internal fun add(first: Double, expectedResult: Double) {
+        assertEquals(expectedResult, squared(first)) {
+            "$first squared should equal $expectedResult"
+        }
+    }
+
+
 }
